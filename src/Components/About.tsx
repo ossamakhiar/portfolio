5c1e00a6bb9b492bf73be76d498146bcab7d8818
Oussama Khiar <ossama.khiar@gmail.com>
2024-04-03 00:55:18 +0000

    Responsive & json data

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,73 +1,20 @@
+import technologies from "../data/techs.json"
+
 
 const About = () => {
-    const technologies = [
-        {
-            id: 1, title: "C++", 
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
-            link: "#"
-        },
-        {
-            id: 3, title: "JavaScript", 
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-            link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-        },
-        {
-            id: 4, title: "TypeScript", 
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
-            link: "https://www.typescriptlang.org/"
-        },
-        {
-            id: 5, title: "Node.js",
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
-            link: "https://nodejs.org"
-        },
-        {
-            id: 6, title: "Express JS",
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
-            link: "https://expressjs.com"
-        },
-        {
-            id: 7, title: "Postgresql",
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
-            link: "https://www.postgresql.org"
-        },
-        {
-            id: 8, title: "Docker",
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
-            link: "https://www.docker.com/"
-        },
-        {
-            id: 9, title: "React JS",
-            img_src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
-            link: "https://reactjs.org/"
-        },
-    ]
-
 
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto items-center">
             <h1 className='text-5xl font-bold text-center mb-3 text-gray-900 dark:text-white'>About me</h1>
-            <p className='text-2xl  text-gray-900 dark:text-white font-medium font-basic'>
+            <p className='text-md md:text-xl  text-gray-900 dark:text-white font-medium font-basic mx-4'>
                 I am a passionate software engineering student at 1337 School, deeply engaged in developing my skills and contributing to 
                 various projects across my curriculum. My journey involves leveraging the power of C/C++ and Node.js to address 
                 a diverse range of challenges. From the intricacies of UNIX programming to the elegance of data structures and algorithms, 
                 I delve into multifaceted domains with enthusiasm. My explorations extend to the vibrant world of web development and mastering tools like Express.js. Additionally, I am delving into the realm of Docker, 
                 seeking to broaden my understanding of containerization and its impact on modern software deployment practices.
             </p>
-            {/* <p className="flex justify-between mt-5">
-                <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="50" height="50"/> </a>
-                <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="50" height="50"/> </a> 
-                <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="50" height="50"/></a>
-                <a href="" target="_blank" rel="noreferrer"> <img src="" alt="javascript" width="50" height="50"/> </a>
-                <a href="" target="_blank" rel="noreferrer"> <img src="" alt="typescript" width="50" height="50"/> </a> 
-                <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="" alt="nodejs" width="50" height="50"/> </a>
-                <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="" alt="express" width="50" height="50"/> </a>
-                <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="50" height="50"/> </a>
-                <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <img src="" alt="docker" width="50" height="50"/> </a>
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="" alt="react" width="50" height="50"/> </a>
-            </p> */}
 
-            <div className="grid grid-cols-4 gap-4 mt-5">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-5 mx-4">
 
                 {technologies.map((tech) => {
                     return (
@@ -81,12 +28,9 @@ const About = () => {
                         </a>
                     );
                 })}
-
-
             </div>
         </div>
     )
-
 };
 
 export default About;
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,21 +1,9 @@
 import { useEffect, useRef } from "react";
+import projects from "../data/projects.json"
+import img from "../assets/netpractice.jpg"
 
 
 const Projects = () => {
-
-    const blogs = [
-        {id: 1,title: "Hello world", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 2,title: "Trenscendance 2024", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 3,title: "Piscine object", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 4,title: "Blah blah", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 5,title: "Blah blah", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 6,title: "Piscine object", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 7,title: "Blah blah", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-        {id: 8,title: "Blah blah", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."},
-    
-    ];
-
-
     const targetsRef = useRef<Element[]>([]);
 
     useEffect(() => {
@@ -46,22 +34,23 @@ const Projects = () => {
 
 
     return (
-        <div className="max-w-7xl mx-auto mt-6">
+        <div className="lg:max-w-7xl mx-auto  mt-6">
             <h1 className='text-5xl font-bold text-center mb-5 text-gray-900 dark:text-white'>Projects</h1>
             
-            <div className="flex flex-wrap gap-4">
-                {blogs.map((blog) => {
+            {/* <div className="flex justify-center"> */}
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-4 items-center">
+                {projects.map((project) => {
                     return (
                         // redundant check
-                    <div key={blog.id} ref={el => el && !targetsRef.current.find((elem) => elem === el) && targetsRef.current.push(el)} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  transition-opacity duration-1000 ease-in-out opacity-0">
+                    <div key={project.id} ref={el => el && !targetsRef.current.find((elem) => elem === el) && targetsRef.current.push(el)} className="sm:w-full md:max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  transition-opacity duration-1000 ease-in-out opacity-0">
                         <a href="#">
-                            <img className="rounded-t-lg" src="" alt="" />
+                            <img className="rounded-t-lg" src={project.img} alt="" />
                         </a>
                         <div className="p-5">
                             <a href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{blog.title}</h5>
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.title}</h5>
                             </a>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{blog.content}</p>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.content}</p>
                             <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Read more
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -73,6 +62,7 @@ const Projects = () => {
 
                     )
                 })}
+            {/* </div> */}
             </div>
         </div>
     )
